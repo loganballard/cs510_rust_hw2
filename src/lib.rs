a5134c6acae65eae53a2a2b5e83bd27a8af578f8
loganballard <loganballard@gmail.com>
2020-01-25 10:21:36 -0800

    got everything working, now to add like command line drivers

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,63 @@
 #[cfg(test)]
 mod tests {
+    use crate::rsa_library::*;
+    use rand::random;
+    use toy_rsa_lib::gcd;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_encrypt() {
+        assert_eq!(encrypt(18282, 32), 6500);
+        assert_eq!(encrypt(55, 3), 53);
+        assert_eq!(encrypt(0xde9c_5816_141c_8ba9, 12345), 0x164e_44b8_6776_d497);
+    }
+
+    #[test]
+    fn test_decrypt() {
+        let msg: u32 = 12345;
+        let p: u32 = 0xed23e6cd;
+        let q: u32 = 0xf050a04d;
+        let pub_key: u64 = 0xde9c5816141c8ba9;
+        let enc = encrypt(pub_key, msg);
+        let dec = decrypt((p, q), enc);
+        assert_eq!(msg, dec);
+    }
+
+    #[test]
+    fn test_genkey() {
+        for _ in 1..10 {
+            let key = genkey();
+            assert!(key.0 > 0);
+            assert!(key.1 > 0);
+            assert_eq!(gcd(key.0 as u64, EXP), 1);
+            assert_eq!(gcd(key.1 as u64, EXP), 1);
+            for i in 1..1000 {
+                assert_eq!(gcd(key.0 as u64, i), 1);
+                assert_eq!(gcd(key.1 as u64, i), 1);
+            }
+        }
+    }
+
+    #[test]
+    fn test_workflow() {
+        for _ in 1..100 {
+            let msg: u32 = random();
+            let key_tup = genkey();
+            let p: u32 = key_tup.0;
+            let q: u32 = key_tup.1;
+            let pub_key: u64 = key_tup.0 as u64 * key_tup.1 as u64;
+            let enc = encrypt(pub_key, msg);
+            let dec = decrypt((p, q), enc);
+            assert_eq!(msg, dec);
+        }
     }
 }
 
 pub mod rsa_library {
-    use toy_rsa_lib::*;
-    use std::convert::TryInto;
     use std::convert::TryFrom;
+    use toy_rsa_lib::*;
+
+    /// Fixed RSA encryption exponent.
+    pub const EXP: u64 = 65537;
 
     #[test]
     fn test_lambda() {
@@ -21,15 +69,6 @@ pub mod rsa_library {
         lcm(x - 1, y - 1)
     }
 
-    /// Fixed RSA encryption exponent.
-    pub const EXP: u64 = 65537;
-
-    #[test]
-    fn test_encrypt() {
-        assert_eq!(encrypt(18282, 32), 6500);
-        assert_eq!(encrypt(55, 3), 53);
-    }
-
     /// Encrypt the plaintext `msg` using the RSA public `key`
     /// and return the ciphertext.
     pub fn encrypt(key: u64, msg: u32) -> u64 {
@@ -37,37 +76,24 @@ pub mod rsa_library {
         modexp(msg_64, EXP, key)
     }
 
-    //#[test]
-    //fn test_decrypt() {
-    //    let key: u64 = 11111;
-    //    let msg: u32 = 12345;
-    //    let enc_val: u64 = encrypt(pub_key, msg);
-    //    let dec_val: u32 = decrypt(pri_key, msg);
-    //}
-
     /// Decrypt the cipertext `msg` using the RSA private `key`
     /// and return the resulting plaintext.
-    pub fn decrypt(key: (u32, u32), msg: u64) -> u64 {
+    pub fn decrypt(key: (u32, u32), msg: u64) -> u32 {
         let first_key_val: u64 = key.0 as u64;
         let sec_key_val: u64 = key.1 as u64;
-        let pub_key: u64 = 0xde9c5816141c8ba9;
-        let d: u64 = modinverse(lambda(first_key_val, sec_key_val), EXP);
-        modexp(msg, d, pub_key)
-        //  TODO - figure out why the decrypted value is so fucked up
-//        let decrypted = modexp(msg, d, pub_key);
-//        let decrypted_32 = u32::try_from(decrypted);
-//        match decrypted_32 {
-//            Ok(_) => (),
-//            Err(e) => panic!("this was the problem: {}", e),
-//        }
-//        decrypted_32.unwrap()
+        let pub_key: u64 = first_key_val * sec_key_val;
+        let d: u64 = modinverse(EXP, lambda(first_key_val, sec_key_val));
+        let decrypted = modexp(msg, d, pub_key);
+        let decrypted_32 = u32::try_from(decrypted);
+        match decrypted_32 {
+            Ok(_) => (),
+            Err(e) => panic!(
+                "this was the problem when converting from u64 -> u32: {}",
+                e
+            ),
+        }
+        decrypted_32.unwrap()
     }
-
-    //    #[test]
-    //    fn test_genkey() {
-    //        let key = genkey();
-    //        assert_eq!(1, 1);
-    //    }
 
     /// Generate a pair of primes in the range `2**30..2**31`
     /// suitable for RSA encryption with exponent
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,12 @@
-use toy_rsa::rsa_library::{genkey, encrypt, decrypt};
+use toy_rsa::rsa_library::{decrypt, encrypt};
 
 fn main() {
     let msg: u32 = 12345;
-//    for i in 0..100 {
-//        let key_pair: (u32, u32) = genkey();
-//        let pub_key = key_pair.0;
-//        let pri_key = key_pair.1 as u64;
-//        let x: u64 = encrypt(pri_key, msg);
-//        let y: u32 = decrypt(key_pair, msg as u64);
-//        println!("{}", y);
-//    }
-    let p: u32 = 0xed23e6cd;
-    let q: u32 = 0xf050a04d;
-    let pub_key: u64 = 0xde9c5816141c8ba9;
+    let p: u32 = 0xed23_e6cd;
+    let q: u32 = 0xf050_a04d;
+    let pub_key: u64 = 0xde9c_5816_141c_8ba9;
     let enc = encrypt(pub_key, msg);
     let dec = decrypt((p, q), enc);
+    println!("p: {}; q: {}; pubkey: {}", p, q, pub_key);
     println!("enc: {}, dec: {}", enc, dec);
 }
